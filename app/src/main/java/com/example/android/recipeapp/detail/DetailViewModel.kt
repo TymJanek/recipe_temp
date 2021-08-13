6762eb34bf04760d3343c34b1e490df785e14bc8
TymJanek <jantanski1@gmail.com>
2021-08-13 15:48:00 +0200

    refactor and deletion of unnecessary comments

--- app/src/main/java/com/example/android/recipeapp/detail/DetailViewModel.kt
+++ app/src/main/java/com/example/android/recipeapp/detail/DetailViewModel.kt
@@ -1,19 +1,3 @@
-/*
- *  Copyright 2018, The Android Open Source Project
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- */
-
 package com.example.android.recipeapp.detail
 
 import android.app.Application
@@ -23,11 +7,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.android.recipeapp.network.RecipeProperty
 
-/**
- * The [ViewModel] that is associated with the [DetailFragment].
- */
 class DetailViewModel(@Suppress("UNUSED_PARAMETER")recipeProperty: RecipeProperty, app: Application) : AndroidViewModel(app) {
-    // TODO (01) Add selected MarsProperty LiveData, and initialize during init, removing @Suppress
     private val _selectedProperty = MutableLiveData<RecipeProperty>()
 
     val selectedProperty: LiveData<RecipeProperty>
@@ -36,5 +16,5 @@ class DetailViewModel(@Suppress("UNUSED_PARAMETER")recipeProperty: RecipePropert
     init {
         _selectedProperty.value = recipeProperty
     }
-    // TODO (18) Add displayPropertyPrice and displayPropertytype LiveData Transformations.map
+
 }
